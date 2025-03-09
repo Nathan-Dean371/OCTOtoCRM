451f9c5fed32976331494ac5f2f76fb4608e2823
Nathan Dean <G00423489@atu.ie>
2025-03-09 22:26:55 +0000

    Add CredentialService: implement encryption and decryption for API keys and sensitive credentials

---
+++ src/services/CredentialService.ts
@@ -0,0 +1,10 @@
+//Encrypt entered credentails like API keys for sources and destinations. 
+//Allow encrypted creadentials to be stored in the database.
+//Allow encrypted credentials to be decrypted for use in the application.
+
+export function encryptCredential(credential: string): string 
+{
+    
+}
+
+
